c31df97f198f0ecec7cb7ac9670b8c444da47862
Oloco2792 <oloco.github@gmail.com>
2025-02-11 12:00:30 -0400

    Ahi vamos

--- login.php
+++ login.php
@@ -3,27 +3,39 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="css/styles.css">
+        <link rel="stylesheet" href="css/styles.css">
+        <link rel="icon" type="image/x-icon" href="img/favicon.ico">
     <title>Iniciar Sesion</title>
 </head>
-<body class="login">
-    <aside class="login-bg">
-        
-    </aside>
-    <main class="login-login">
-        <div class="caja">
-            <h1>Inicio de Sesión</h1>
-            <form class="formulario-Login" action="/action_page.php">
-                <label for="usuarioLogin">Usuario</label>
-                <input type="text" id="usuarioLogin" name="usuarioLogin">
-                <label for="contraseñaLogin">Contraseña</label>
-                <input type="text" id="contraseñaLogin" name="contraseñaLogin">
-                <input type="submit" value="ingresar">
-            </form>
+<body>
+    <div class="login">
+        <div class="login-bg">
+            <aside class="grey-out width100">
+                <h2>Texto Generico Para Probar</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat.</p>
+            </aside>
         </div>
-    </main>
+        <main class="login-login">
+            <div class="caja">
+                <h1>Inicio de Sesión</h1>
+                <form class="formulario-Login" action="/action_page.php">
+                    <label for="usuarioLogin">Usuario</label>
+                    <input type="text" id="usuarioLogin" name="usuarioLogin">
+                    <label for="contraseñaLogin">Contraseña</label>
+                    <input type="text" id="contraseñaLogin" name="contraseñaLogin">
+                    <input type="submit" value="ingresar">
+                </form>
+            </div>
+        </main>
+    </div>
+    <footer class="pheight">
+        <p>"Ningun plan sobrevive al contacto con el enemigo"</p>
+    </footer>
 </body>
 </html>
