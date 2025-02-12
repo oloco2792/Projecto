291511c7ea709a7a7b11561d2c0c40f0def62adf
oloco2792 <oloco.github@gmail.com>
2025-02-12 19:55:00 -0400

    Solucionado el problema de la 'caja chica'

--- login.php
+++ login.php
@@ -3,27 +3,32 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="css/styles.css">
+            <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+        <link rel="preconnect" href="css/normalize.css" as="style">
+            <link rel="stylesheet" href="css/normalize.css">
+        <link rel="preconnect" href="css/styles.css" as="style">
+            <link rel="stylesheet" href="css/styles.css">
+
         <link rel="icon" type="image/x-icon" href="img/favicon.ico">
     <title>Iniciar Sesion</title>
 </head>
 <body>
     <div class="login">
-        <div class="login-bg">
-            <aside class="grey-out width100">
-                <h2>Texto Generico Para Probar</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+        <div class="login__bg">
+            <aside class="hero grey-out height100">
+                <h1 class="hero__titulo centrar-texto">Texto Generico Para Probar</h1>
+                <p class="hero__texto">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
                 tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
                 quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
                 consequat.</p>
             </aside>
         </div>
-        <main class="login-login">
-            <div class="caja">
-                <h1>Inicio de Sesión</h1>
+        <main class="login__form">
+            <div class="formulario caja">
+                <h2>Inicio de Sesión</h2>
                 <form class="formulario-Login" action="/action_page.php">
                     <label for="usuarioLogin">Usuario</label>
                     <input type="text" id="usuarioLogin" name="usuarioLogin">
@@ -34,8 +39,10 @@
             </div>
         </main>
     </div>
-    <footer class="pheight">
-        <p>"Ningun plan sobrevive al contacto con el enemigo"</p>
-    </footer>
+    <!--<footer class="footer">
+        <div>
+            <p>"Ningun plan sobrevive al contacto con el enemigo"</p>
+        </div>
+    </footer>-->
 </body>
 </html>
